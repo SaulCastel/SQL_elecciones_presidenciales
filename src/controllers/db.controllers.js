3c8548847cb908750eb9078614da2ff22803236f
Saul Castellanos <saulcastel018@gmail.com>
2023-09-15 12:23:46 -0600

    Queries para cargar todos los datos a todas las tablas

--- src/controllers/db.controllers.js
+++ src/controllers/db.controllers.js
@@ -11,7 +11,8 @@ export const createModel = async (req, res) => {
     const script = fs.readFileSync(path.join(__dirname, '../db/database.sql'), 'utf8')
     const queries = script.split(';').map(query => query.trim())
     const conn = await createConnection(db)
-    for (let i = 0; i < queries.length-1; i++) {
+    for (let i = 0; i < queries.length; i++) {
+        if (queries[i].length === 0 ) continue
         await conn.query(queries[i])
     }
     await conn.end()
@@ -44,46 +45,55 @@ export const loadData = async (req, res) => {
     const script = fs.readFileSync(path.join(__dirname, '../db/temporary.sql'), 'utf8')
     const queries = script.split(';').map(query => query.trim())
     var conn = await createConnection(db)
-    for (let i = 0; i < queries.length-1; i++) {
+    for (let i = 0; i < queries.length; i++) {
+        if (queries[i].length === 0 ) continue
         await conn.query(queries[i])
     }
+    console.log('Tablas temporales creadas')
     let csv, rows, query
 
     //Cargar datos de tabla 'cargo'
     csv = path.join(__dirname, `../../TSEdatasets/cargos.csv`)
     rows = fs.readFileSync(csv, 'utf8').split('\n')
     query = getQueries('cargo',['id_cargo','cargo'])
-    for (let i = 1; i<rows.length-1; i++) {
+    for (let i = 1; i<rows.length; i++) {
+        if (rows[i].length === 0) continue
         const fields = rows[i].split(',').map(value => value.trim())
         await conn.query(query.temp,fields)
     }
     await conn.query(query.final)
+    console.log('Tabla cargo')
 
     //Cargar datos de tabla 'departamento'
     csv = path.join(__dirname, `../../TSEdatasets/departamentos.csv`)
     rows = fs.readFileSync(csv, 'utf8').split('\n')
     query = getQueries('departamento',['id_dep','nombre'])
-    for (let i = 1; i<rows.length-1; i++) {
+    for (let i = 1; i<rows.length; i++) {
+        if (rows[i].length === 0) continue
         const fields = rows[i].split(',').map(value => value.trim())
         await conn.query(query.temp,fields)
     }
     await conn.query(query.final)
+    console.log('Tabla departamento')
 
     //Cargar datos de tabla 'ciudadano'
     csv = path.join(__dirname, `../../TSEdatasets/ciudadanos.csv`)
     rows = fs.readFileSync(csv, 'utf8').split('\n')
     query = getQueries('ciudadano',['dpi','nombre','apellido','direccion','telefono','edad','genero'])
-    for (let i = 1; i<rows.length-1; i++) {
+    for (let i = 1; i<rows.length; i++) {
+        if (rows[i].length === 0) continue
         const fields = rows[i].split(',').map(value => value.trim())
         await conn.query(query.temp,fields.slice(0,7))
     }
     await conn.query(query.final)
+    console.log('Tabla ciudadano')
 
     //Cargar datos de tabla 'partido'
     csv = path.join(__dirname, `../../TSEdatasets/partidos.csv`)
     rows = fs.readFileSync(csv, 'utf8').split('\n')
     query = getQueries('partido',['id_partido','nombre','siglas','fundacion'])
-    for (let i = 1; i<rows.length-1; i++) {
+    for (let i = 1; i<rows.length; i++) {
+        if (rows[i].length === 0) continue
         const fields = rows[i].trim().match(/(?=.+,?)([-0-9A-zÀ-ú/ ]+|"[^"]+"|)/g)
         //const fields = rows[i].split(',').map(value => value.trim())
         const date = fields[6].split('/')
@@ -91,6 +101,64 @@ export const loadData = async (req, res) => {
         await conn.query(query.temp,[fields[0],fields[2].replaceAll('"',''),fields[4],fields[6]])
     }
     await conn.query(query.final)
+    console.log('Tabla partido')
+
+    //Cargar datos de tabla 'candidato'
+    csv = path.join(__dirname, `../../TSEdatasets/candidatos.csv`)
+    rows = fs.readFileSync(csv, 'utf8').split('\n')
+    query = getQueries('candidato',['id_candidato','nombre','nacimiento','id_partido','id_cargo'])
+    for (let i = 1; i<rows.length; i++) {
+        if (rows[i].length === 0) continue
+        const fields = rows[i].split(',').map(value => value.trim())
+        const date = fields[2].split('/')
+        fields[2]= `${date[2]}-${date[1]}-${(date[0].length === 1) ? '0'+date[0]:date[0]}`
+        await conn.query(query.temp,fields)
+    }
+    await conn.query(query.final)
+    console.log('Tabla candidato')
+
+    //Cargar datos de tabla 'mesa'
+    csv = path.join(__dirname, `../../TSEdatasets/mesas.csv`)
+    rows = fs.readFileSync(csv, 'utf8').split('\n')
+    query = getQueries('mesa',['id_mesa','id_dep'])
+    for (let i = 1; i<rows.length; i++) {
+        if (rows[i].length === 0) continue
+        const fields = rows[i].split(',').map(value => value.trim())
+        await conn.query(query.temp,fields)
+    }
+    await conn.query(query.final)
+    console.log('Tabla mesa')
+
+    //Cargar datos de tabla 'voto' y 'detalle_voto'
+    csv = path.join(__dirname, `../../TSEdatasets/votaciones.csv`)
+    rows = fs.readFileSync(csv, 'utf8').split('\n')
+    query = getQueries('voto',['id_voto','id_candidato','dpi','id_mesa','fecha_hora'])
+    for (let i = 1; i<rows.length; i++) {
+        if (rows[i].length === 0) continue
+        const fields = rows[i].split(',').map(value => value.trim())
+        const date_time = fields[4].split('/')
+        const year_time = date_time[2].split(' ')
+        const dateStr = `${year_time[0]}-${date_time[1]}-${(date_time[0].length === 1) ? '0'+date_time[0]:date_time[0]}`
+        const time = year_time[1].split(':')
+        const timeStr = `${(time[0].length === 1) ? '0'+time[0]:time[0]}:${time[1]}:00`
+        fields[4]= dateStr+' '+timeStr
+        await conn.query(query.temp, fields)   
+    }
+    const voteQuery = `
+        INSERT INTO ${DB_DATABASE}.voto (id_voto, dpi, id_mesa, fecha_hora)
+        SELECT DISTINCT temp.id_voto, temp.dpi, temp.id_mesa, temp.fecha_hora
+        FROM ${DB_DATABASE}.temp_voto temp
+        LEFT JOIN ${DB_DATABASE}.voto v ON temp.id_voto = v.id_voto
+        WHERE v.id_voto IS NULL
+    `
+    await conn.query(voteQuery)
+    console.log('Tabla voto')
+    const detailQuery = `
+        INSERT INTO ${DB_DATABASE}.detalle_voto (id_voto, id_candidato)
+        SELECT id_voto, id_candidato FROM ${DB_DATABASE}.temp_voto
+    `
+    await conn.query(detailQuery)
+    console.log('Tabla detalle_voto')
 
     await conn.end()
     res.send("Datos cargados en el modelo")
