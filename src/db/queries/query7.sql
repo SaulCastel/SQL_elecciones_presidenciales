668fbe4a10ef6c90cb9627b826bc41d2f09daa46
Saul Castellanos <saulcastel018@gmail.com>
2023-09-17 15:35:01 -0600

    Arreglo a query7 para order por conteo y no por edad

--- src/db/queries/query7.sql
+++ src/db/queries/query7.sql
@@ -1,6 +1,6 @@
 SELECT c.edad as edad, COUNT(*) as conteo
-FROM proy1bases1.voto v
-JOIN proy1bases1.ciudadano c ON v.dpi = c.dpi 
+FROM proy1bases1.ciudadano c
+JOIN proy1bases1.voto v ON v.dpi = c.dpi 
 GROUP BY edad
-ORDER BY edad DESC
+ORDER BY conteo DESC
 LIMIT 10;
